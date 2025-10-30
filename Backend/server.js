75d59151fe86bc7e6c48c34acc2941acea97b767
Kirti Tripathi <98109418+Rashi-Kirti06@users.noreply.github.com>
2025-10-30 23:14:16 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,4 @@ app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running successfully 🚀");
-});
+
